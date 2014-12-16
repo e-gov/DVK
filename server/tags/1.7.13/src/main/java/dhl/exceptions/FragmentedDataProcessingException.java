5b2b5bcdd2bd5b94113c4b4ebe4cc25f0a2ea01b
lauriesko <lauriesko@b82749c9-0539-460a-b78e-f605a76e0c92>
2014-12-16 14:56:50 +0000

    1. Changed connection to database 2. Changed all procedures for postreSQL 3. Added new jar file postresql-9.0.801 in pom.xml 4. Added script server_latest_pgsql.sql for creating postreSQL database(table, sequence, functions and others) 5. Added two pgsql scripts for testing projects (insert_data_pgsql and insert_data2_pgsql) 6. Added txt file paringute_kirjeldus, he show what services are tested 7. Added script server_data_pgsql, after import to database, you can testing all soap request ( ../sql/testdata/soapxml/)

---
+++ server/tags/1.7.13/src/main/java/dhl/exceptions/FragmentedDataProcessingException.java
@@ -0,0 +1,21 @@
+package dhl.exceptions;
+
+public class FragmentedDataProcessingException extends Exception {
+    private static final long serialVersionUID = -7036989158526879014L;
+
+    public FragmentedDataProcessingException() {
+        super();
+    }
+
+    public FragmentedDataProcessingException(String message) {
+        super(message);
+    }
+
+    public FragmentedDataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FragmentedDataProcessingException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ server/tags/1.7.13/src/main/java/dhl/iostructures/receiveDocumentsV3RequestType.java
@@ -0,0 +1,111 @@
+package dhl.iostructures;
+
+import dvk.core.CommonMethods;
+import dvk.core.Settings;
+
+import java.util.ArrayList;
+import javax.xml.soap.SOAPBody;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class receiveDocumentsV3RequestType {
+    static Logger logger = Logger.getLogger(receiveDocumentsV3RequestType.class.getName());
+    public int arv;
+    public int allyksus;
+    public int ametikoht;
+    public ArrayList<String> kaust;
+    public String edastusID;
+    public int fragmentNr;
+    public long fragmentSizeBytes;
+    public long fragmentSizeBytesOrig;
+
+    public receiveDocumentsV3RequestType() {
+        arv = 10;
+        allyksus = 0;
+        ametikoht = 0;
+        kaust = new ArrayList<String>();
+        edastusID = "";
+        fragmentNr = -1;
+        fragmentSizeBytes = Settings.Client_FragmentSizeBytes;
+        fragmentSizeBytesOrig = 0;
+    }
+
+    public static receiveDocumentsV3RequestType getFromSOAPBody(org.apache.axis.MessageContext context) {
+        try {
+            org.apache.axis.Message msg = context.getRequestMessage();
+            SOAPBody body = msg.getSOAPBody();
+            NodeList nodes = body.getElementsByTagName("receiveDocuments");
+            if (nodes.getLength() > 0) {
+                Element el = (Element) nodes.item(0);
+                nodes = el.getElementsByTagName("keha");
+                if (nodes.getLength() > 0) {
+                    receiveDocumentsV3RequestType result = new receiveDocumentsV3RequestType();
+                    Element bodyNode = (Element) nodes.item(0);
+
+                    // Arv
+                    result.arv = CommonMethods.getNumberFromChildNode(bodyNode, "arv", 10);
+                    // Kontrollime, et ei saaks esitada päringut, mille väljundisse
+                    // ei ole lubatud ühtegi dokumenti.
+                    if (result.arv < 1) {
+                        result.arv = 1;
+                    }
+
+                    // Allüksus
+                    result.allyksus = CommonMethods.getNumberFromChildNode(bodyNode, "allyksus", 0);
+
+                    // Ametikoht
+                    result.ametikoht = CommonMethods.getNumberFromChildNode(bodyNode, "ametikoht", 0);
+
+                    // Kaust
+                    nodes = bodyNode.getElementsByTagName("kaust");
+                    if (nodes.getLength() > 0) {
+                        for (int i = 0; i < nodes.getLength(); ++i) {
+                            Element folderNode = (Element) nodes.item(i);
+                            result.kaust.add(CommonMethods.getNodeText(folderNode));
+                        }
+                    }
+
+                    // Edastus ID
+                    nodes = bodyNode.getElementsByTagName("edastus_id");
+                    if (nodes.getLength() > 0) {
+                        result.edastusID = CommonMethods.getNodeText((Element) nodes.item(0)).trim();
+                    }
+
+                    // Fragmendi nr
+                    nodes = bodyNode.getElementsByTagName("fragment_nr");
+                    if (nodes.getLength() > 0) {
+                        try {
+                            result.fragmentNr = Integer.parseInt(CommonMethods.getNodeText((Element) nodes.item(0)));
+                        } catch (Exception ex) {
+                            CommonMethods.logError(ex, "dhl.iostructures.receiveDocumentsV2RequestType", "getFromSOAPBody");
+                        }
+                    }
+
+                    // Fragmendi suurus
+                    nodes = bodyNode.getElementsByTagName("fragmendi_suurus_baitides");
+                    if (nodes.getLength() > 0) {
+                        try {
+                            result.fragmentSizeBytesOrig = Long.parseLong(CommonMethods.getNodeText((Element) nodes.item(0)));
+                        } catch (Exception ex) {
+                            result.fragmentSizeBytesOrig = 0;
+                        }
+                    }
+                    // Fragmendi suurust alla 100kB ei luba
+                    if (result.fragmentSizeBytesOrig < (1024 * 100)) {
+                        result.fragmentSizeBytes = 1024 * 100;
+                    } else {
+                        result.fragmentSizeBytes = result.fragmentSizeBytesOrig;
+                    }
+
+                    return result;
+                }
+            }
+            return null;
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return null;
+        }
+    }
+}

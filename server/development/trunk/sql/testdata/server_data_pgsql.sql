0ca5e13fa46a06fac9f8604ba1a6cf8a54a9c978
lauriesko <lauriesko@b82749c9-0539-460a-b78e-f605a76e0c92>
2014-12-31 06:46:43 +0000

    Added insert data for testing

--- server/development/trunk/sql/testdata/server_data_pgsql.sql
+++ server/development/trunk/sql/testdata/server_data_pgsql.sql
@@ -1,6 +1,30 @@
 SET dvkxtee.xtee_asutus = '';
 SET dvkxtee.xtee_isikukood = '';
 
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (1, 'Dokumente on puudu (Pooleli)');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (2, 'Järjekorras');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (3, 'Ootel');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (4, 'Lõpetatud');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (5, 'Tagasi lükatud');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (6, 'Teha');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (7, 'Töötlemisel');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (8, 'Aktsepteeritud (Võetud töösse)');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (9, 'Salvestatud');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (10, 'Arhiveeritud');
+INSERT INTO vastuvotja_staatus(vastuvotja_staatus_id, nimetus) VALUES  (11, 'Saadetud');
+
+INSERT INTO klassifikaatori_tyyp(klassifikaatori_tyyp_id, nimetus) VALUES  (1,'Saatmisviis');
+INSERT INTO klassifikaatori_tyyp(klassifikaatori_tyyp_id, nimetus) VALUES  (2,'Dokumendi olek');
+
+INSERT INTO klassifikaator(klassifikaator_id, nimetus, klassifikaatori_tyyp_id) VALUES  (1, 'xtee', 1);
+INSERT INTO klassifikaator(klassifikaator_id, nimetus, klassifikaatori_tyyp_id) VALUES  (2, 'epost', 1);
+INSERT INTO klassifikaator(klassifikaator_id, nimetus, klassifikaatori_tyyp_id) VALUES  (101, 'saatmisel', 2);
+INSERT INTO klassifikaator(klassifikaator_id, nimetus, klassifikaatori_tyyp_id) VALUES  (102, 'saadetud', 2);
+INSERT INTO klassifikaator(klassifikaator_id, nimetus, klassifikaatori_tyyp_id) VALUES  (103, 'katkestatud', 2);
+
+INSERT INTO parameetrid(aar_viimane_sync) VALUES (current_date);
+
+
 INSERT INTO dvk.asutus (registrikood, nimetus, liik2, dhl_saatmine, dhs_nimetus) 
 VALUES ('70006317', 'RIA', null, '1', null);
 

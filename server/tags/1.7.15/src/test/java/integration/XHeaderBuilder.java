0f65dff063e39733991e0f4a7f0031fe7c1296a5
lauriesko <lauriesko@b82749c9-0539-460a-b78e-f605a76e0c92>
2015-01-14 09:06:59 +0000

    Muudetu server pom.xml, nagu kirjeldatud paigaldusjendis
    Former-commit-id: 6f44cfdc27b1d65a545840588f3087e9758d29c9

---
+++ server/tags/1.7.15/src/test/java/integration/XHeaderBuilder.java
@@ -0,0 +1,56 @@
+package integration;
+
+import dhl.iostructures.XHeader;
+
+/**
+ * {@link dhl.iostructures.XHeader} builder class for simpler testing purposes.
+ */
+class XHeaderBuilder {
+    private String asutus;
+    private String andmekogu;
+    private String ametnik;
+    private String id;
+    private String nimi;
+    private String toimik;
+    private String isikukood;
+
+    public XHeaderBuilder setAsutus(String asutus) {
+        this.asutus = asutus;
+        return this;
+    }
+
+    public XHeaderBuilder setAndmekogu(String andmekogu) {
+        this.andmekogu = andmekogu;
+        return this;
+    }
+
+    public XHeaderBuilder setAmetnik(String ametnik) {
+        this.ametnik = ametnik;
+        return this;
+    }
+
+    public XHeaderBuilder setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public XHeaderBuilder setNimi(String nimi) {
+        this.nimi = nimi;
+        return this;
+    }
+
+    public XHeaderBuilder setToimik(String toimik) {
+        this.toimik = toimik;
+        return this;
+    }
+
+    public XHeaderBuilder setIsikukood(String isikukood) {
+        this.isikukood = isikukood;
+        return this;
+    }
+
+    public XHeader build() {
+        return new XHeader(
+                asutus, andmekogu, ametnik, id, nimi, toimik, isikukood);
+    }
+}
